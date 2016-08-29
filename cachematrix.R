1339012ec03bc7096537e2543d6a966a7b8a0e15
chugachhasaPoint <samantha.neeno@optum.com>
2016-08-29 00:33:23 -0400

    Assignment 2: Submit my work on cacheMatrix
    My work for the 3rd week of R Programming. A sample of working this code: 
    
    > trialM = matrix(c(2,3,3,4),nrow=2,ncol=2)
    > m1 = makeCacheMatrix(trialM)
    > cacheSolve(m1)
         [,1] [,2]
    [1,]   -4    3
    [2,]    3   -2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following functions will cache and generate the inverse of a supplied matrix
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+##  This function creates a special "matrix" object that can cache its inverse
+makeCacheMatrix <- function(x = matrix()) { 
+  m <- NULL
+  set <- function(y = matrix) {
+    ## <<- assigns the variable to the parent environment
+    ## in this case, it is the global environment
+      ## For some reason demanded that I expicitly define its input
+      ## as a matrix, otherwise it kept assuming it was a vector
+    x <<- y
+      ## x is the input vector, stored in global memory
+    m <<- NULL
+      ## a flag that changes depending on value storage
+  }
+  get <- function() x
+    ## returns the value of x the matrix
+  setIMATRIX <- function(solve) m <<- solve
+    # Here I'm assigning a function to use the function solve
+  # to compute the inverse (THANK YOU R)
+  # This operation depends on whether another variable is a NA 
+  # (serving as a flag, essentially)
+  
+  getIMATRIX <- function() m
+  list(set = set, # the input matrix will be assigned as defined
+       get = get, # same with the retreival of the input 
+       setIMATRIX = setIMATRIX,  # solving the matrix
+       getIMATRIX = getIMATRIX)
+  
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+## This function returns a matrix that is the inverse of 'x'
+##  If the inverse has already been calculated (and the matrix 
+## has not changed), then the cachesolve should retrieve the inverse from the cache
+cacheSolve <- function(x = matrix(),...){
+  m <- x$getIMATRIX()
+  ## performs a function on m, and assigns its return as m in the local environment
+  
+  if(!is.null(m)) {
+    message("getting cached data--in this case, the matrix!")
+    ## The condition where m == NA is when 
+    return(m)
+  }
+  dataMATRIX <- x$get()
+  m <- solve(dataMATRIX, ...)
+  x$setIMATRIX(m)
+  m
+    }
